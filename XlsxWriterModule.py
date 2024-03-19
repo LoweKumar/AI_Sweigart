8986177270d77c7dc52af1c6be3eeb78ee365229
Lowe Kumar <lowekr00@gmail.com>
2024-03-19 17:21:58 +0530

    Create a workbook and add a worksheet

--- XlsxWriterModule.py
+++ XlsxWriterModule.py
@@ -1,8 +1,41 @@
 import xlsxwriter
 
 # Running a sample program
-workbook = xlsxwriter.Workbook('hello.xlsx')
+# workbook = xlsxwriter.Workbook('hello.xlsx')
+# worksheet = workbook.add_worksheet()
+# worksheet.write('A1', 'HelloWorld')
+# workbook.close()
+
+# Create a workbook and add a worksheet.
+# https://xlsxwriter.readthedocs.io/tutorial01.html
+
+workbook = xlsxwriter.Workbook('Expenses01.xlsx')
 worksheet = workbook.add_worksheet()
-worksheet.write('A1', 'HelloWorld')
+
+# Some data we want to write to the worksheet.
+expenses = (
+    ['Rent', 1000],
+    ['Gas',   100],
+    ['Food',  300],
+    ['Gym',    50],
+)
+
+# Start from the first cell. Rows and columns are zero indexed.
+row = 0
+col = 0
+
+# Iterate over the data and write it out row by row.
+for item, cost in (expenses):
+    worksheet.write(row, col, item)
+    worksheet.write(row, col+1, cost)
+    row+=1
+
+# Write a total using a formula.
+worksheet.write(row, 0, 'Total')
+worksheet.write(row, 1, '=SUM(B1:B4)')
+
 workbook.close()
+
+
+
 
